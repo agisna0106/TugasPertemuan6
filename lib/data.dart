5f7a1193b31c10591f515baec6a79cb693c4cc85
agisna <sarifan176@gmail.com>
2025-11-16 20:09:11 +0700

    add: 10 list wisata

--- lib/data.dart
+++ lib/data.dart
@@ -25,5 +25,30 @@ class Wisata{
       'image': 'https://pbs.twimg.com/media/GA_ftgfaAAA5AxH.png',
       'desc' : 'Roti O Lempuyangan adalah salah satu tempat terkenal di Yogyakarta yang menyajikan roti panggang dengan isian krim susu yang lezat. Tempat ini sering dikunjungi oleh wisatawan dan penduduk lokal yang ingin menikmati camilan manis yang hangat dan nikmat.'
     },
+    {
+      'title': 'Pabrik Superstar Jumbo',
+      'image': 'https://i.ytimg.com/vi/NrBMOztgTk8/maxresdefault.jpg',
+      'desc' : 'Pabrik Superstar Jumbo adalah fasilitas produksi roti dan kue yang terkenal di Indonesia. Pabrik ini dikenal dengan produk-produk berkualitas tinggi dan inovatif, serta proses produksi yang higienis dan efisien. Pengunjung dapat melihat langsung proses pembuatan roti dan kue di pabrik ini.'
+    },
+    {
+      'title': 'Perkebunan Apel Hijau Malang',
+      'image': 'https://cdn.pixabay.com/photo/2015/10/03/20/52/apple-970352_1280.jpg',
+      'desc' : 'Perkebunan Apel Hijau di Malang adalah destinasi wisata agrikultur yang menawarkan pengalaman unik bagi pengunjung untuk memetik apel langsung dari pohonnya. Perkebunan ini tidak hanya menyediakan buah apel segar, tetapi juga pemandangan alam yang indah dan udara yang sejuk, menjadikannya tempat yang ideal untuk rekreasi keluarga.'
+    },
+    {
+      'title': 'Penangkaran Kodok Amazon',
+      'image': 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSLngFH7qV5IsLIML3LozYVtANxf9ecDXtlBA&s',
+      'desc' : 'Penangkaran Kodok Amazon adalah fasilitas konservasi yang bertujuan untuk melindungi dan melestarikan spesies kodok yang berasal dari hutan hujan Amazon. Penangkaran ini menyediakan lingkungan yang mirip dengan habitat aslinya, memungkinkan kodok untuk berkembang biak dan hidup dengan baik. Selain itu, penangkaran ini juga berfungsi sebagai pusat edukasi bagi pengunjung tentang pentingnya menjaga keanekaragaman hayati.'
+    },
+    {
+      'title': 'Bengkel RWB Gus Akira',
+      'image': 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSchb9ktrnychuzLWKT63MFpSEaQU2SXx2bJw&s',
+      'desc' : 'Bengkel RWB Gus Akira adalah tempat di mana mobil-mobil klasik dan modifikasi mendapatkan perawatan dan sentuhan khusus. Dikenal karena keahliannya dalam restorasi dan modifikasi kendaraan, bengkel ini menarik perhatian para penggemar otomotif yang ingin memberikan tampilan unik pada mobil mereka.'
+    },
+    {
+      'title': 'Barak Militer Pelatihan Ketat',
+      'image': 'https://i.ytimg.com/vi/ZONctwDho4Y/maxresdefault.jpg',
+      'desc' : 'Barak Militer Pelatihan Ketat adalah fasilitas tempat para prajurit menjalani pelatihan fisik dan mental yang intensif. Di barak ini, para tentara dilatih untuk menghadapi berbagai situasi dan tantangan yang mungkin mereka hadapi di lapangan. Fasilitas ini dilengkapi dengan peralatan latihan modern dan didukung oleh instruktur berpengalaman untuk memastikan kesiapan optimal para prajurit.'
+    },
   ];
 }
